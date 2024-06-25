c9b50081a6fd293ce1cd599075f29b6dcc228c41
sonic_72_kon <kko160305@mail.ru>
2024-06-25 14:07:34 +0500

    Changed the approach to router registration

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,11 +1,12 @@
 """Импортируются все роутеры и добавляются в список routers_list."""
 
 from .admin import admin_routers
-from .user import user_routers
+from .user import user_routers, search_routers
 
 routers_list = [
-    admin_routers,
     user_routers,
+    admin_routers,
+    search_routers,
 ]
 
 # __all__ = [
--- tgbot/handlers/user/__init__.py
+++ tgbot/handlers/user/__init__.py
@@ -4,15 +4,17 @@ from .base_store import base_store_routers
 from .booking import booking_routers
 from .cancel_booking import cancel_booking_routers
 from .search import search_routers
+from .share_base import share_base_routers
 from .start import start_router
 from .support import support_routers
 
 user_routers = Router()
 user_routers.include_routers(
-    start_router,
+    start_router,  # Должен быть первым
     base_store_routers,
     booking_routers,
     cancel_booking_routers,
+    share_base_routers,
     support_routers,
-    search_routers,
+    # search_routers,  # Должен быть последним
 )
