9899b2f60ba1c33dacd9634213e652b7b26cca9f
sonic_72_kon <kko160305@mail.ru>
2024-06-25 12:26:38 +0500

    Fixed a bug with importing keyboards

--- tgbot/keyboards/inline/__init__.py
+++ tgbot/keyboards/inline/__init__.py
@@ -19,28 +19,28 @@ __all__ = (
     "support_answer_keyboard",
 )
 
-from action_buttons import (
+from .action_buttons import (
     back_and_cancel_keyboard,
     cancel_keyboard,
     done_cancel_keyboard,
     yes_and_cancel_keyboard,
 )
-from add_book import (
+from .add_book import (
     demo_post_keyboard,
     done_clear_back_cancel_keyboard,
     prices_post_keyboard,
 )
-from base_store import (
+from .base_store import (
     confirm_exchange_keyboard,
     discounts_keyboard,
     exchange_base_keyboard,
 )
-from booking import (
+from .booking import (
     booking_again_keyboard,
     booking_from_user_keyboard,
     show_booking_cancel_keyboard,
 )
-from buy import deep_link_buy_keyboard
-from edit_book import edit_keyboard, prices_keyboard
-from search import pagination_keyboard
-from support import support_answer_keyboard
+from .buy import deep_link_buy_keyboard
+from .edit_book import edit_keyboard, prices_keyboard
+from .search import pagination_keyboard
+from .support import support_answer_keyboard
