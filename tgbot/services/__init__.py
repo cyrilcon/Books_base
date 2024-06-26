81387e106ff6aadbb0748ef47de3466f102a3165
sonic_72_kon <kko160305@mail.ru>
2024-06-26 12:52:55 +0500

    The levenshtein_search functions are combined into one file

--- tgbot/services/__init__.py
+++ tgbot/services/__init__.py
@@ -17,8 +17,7 @@ from .check_username import check_username
 from .fluent_loader import get_fluent_localization
 from .forming_text import forming_text
 from .get_language import get_user_language
-from .levenshtein_search import levenshtein_search
-from .levenshtein_search_one_book import levenshtein_search_one_book
+from .levenshtein_search import levenshtein_search, levenshtein_search_one_book
 from .process_search import process_search
 from .set_bot_commands import set_default_commands
 from .url_user import get_url_user
--- tgbot/services/levenshtein_search.py
+++ tgbot/services/levenshtein_search.py
@@ -32,3 +32,29 @@ async def levenshtein_search(search_text, list_of_values):
         return None
 
     return matching
+
+
+async def levenshtein_search_one_book(search_text, list_of_values):
+    """
+    Поиск книги по расстоянию Левенштейна.
+    :param search_text: Текст, который вводится для поиска.
+    :param list_of_values: Список со всеми книгами.
+    :return: Название книги с наилучшим совпадением.
+    """
+
+    best_match = None
+    best_ratio = 0
+
+    search_text_lower = search_text.lower()
+
+    for current_text in list_of_values:
+        current_text_lower = current_text.lower()
+        current_ratio = fuzz.partial_ratio(current_text_lower, search_text_lower)
+        if current_ratio > best_ratio:
+            best_ratio = current_ratio
+            best_match = current_text
+
+    if best_ratio >= 90:
+        return best_match
+    else:
+        return None
