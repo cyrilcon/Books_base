dce3d92d688f172902c966dddafd0d19bb1e12ca
sonic_72_kon <kko160305@mail.ru>
2024-09-13 12:17:13 +0500

    Added convert_datetime

---
+++ tgbot/services/convert_utc_datetime.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timezone
+
+from tgbot.config import config
+
+
+def convert_utc_datetime(utc_datetime: datetime):
+    return (
+        utc_datetime.replace(tzinfo=timezone.utc)
+        .astimezone(config.misc.yekaterinburg_timezone)
+        .strftime("%Y-%m-%d %H:%M:%S")
+    )
