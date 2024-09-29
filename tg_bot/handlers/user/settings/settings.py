e37da40068dae7dd377f50477db3b267050f5ce2
sonic_72_kon <kko160305@mail.ru>
2024-09-29 12:49:23 +0500

    Updated settings command

--- tg_bot/handlers/user/settings/settings.py
+++ tg_bot/handlers/user/settings/settings.py
@@ -5,7 +5,7 @@ from fluent.runtime import FluentLocalization
 
 from api.books_base_api import api
 from tg_bot.keyboards.inline import set_language_keyboard
-from tg_bot.services import get_user_localization
+from tg_bot.services import get_fluent_localization
 
 settings_router = Router()
 
@@ -19,26 +19,29 @@ async def settings(message: Message, l10n: FluentLocalization):
 
 
 @settings_router.callback_query(F.data.startswith("set_language"))
-async def settings_set_language(
-    call: CallbackQuery,
-    l10n: FluentLocalization,
-):
+async def settings_set_language(call: CallbackQuery, l10n: FluentLocalization):
+    new_language_code = call.data.split(":")[-1]
+
     id_user = call.from_user.id
     response = await api.users.get_user_by_id(id_user)
     user = response.get_model()
 
-    new_language_code = call.data.split(":")[-1]
-
-    if user.language_code != new_language_code:
-        await api.users.update_user(id_user=id_user, language_code=new_language_code)
-        l10n = await get_user_localization(id_user)
-
-        await call.message.edit_text(
-            l10n.format_value("settings-success"),
-            reply_markup=set_language_keyboard(l10n),
-        )
-        await call.answer()
-    else:
+    if user.language_code == new_language_code:
         await call.answer(
-            l10n.format_value("settings-error-already-set"), show_alert=True
+            l10n.format_value("settings-error-language-already-set"),
+            show_alert=True,
         )
+        return
+
+    response = await api.users.update_user(
+        id_user=id_user,
+        language_code=new_language_code,
+    )
+    user = response.get_model()
+    l10n = get_fluent_localization(user.language_code)
+
+    await call.message.edit_text(
+        l10n.format_value("settings-success"),
+        reply_markup=set_language_keyboard(l10n),
+    )
+    await call.answer()
