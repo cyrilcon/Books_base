d7e970a3e9a61e6016a7871d5d9868fea6646cf8
sonic_72_kon <kko160305@mail.ru>
2024-10-10 14:27:06 +0500

    Updated read

--- tg_bot/handlers/user/payment/__init__.py
+++ tg_bot/handlers/user/payment/__init__.py
@@ -4,9 +4,11 @@ from aiogram import Router
 
 from .book import payment_book_router
 from .premium import payment_premium_router
+from .read import read_router
 
 payment_routers = Router()
 payment_routers.include_routers(
     payment_book_router,
     payment_premium_router,
+    read_router,
 )
