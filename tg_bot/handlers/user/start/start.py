6f97cc6c5f876cd53f6726b767e92884ce7ecb7d
sonic_72_kon <kko160305@mail.ru>
2024-10-11 20:25:42 +0500

    Updated user commands when start command sent

--- tg_bot/handlers/user/start/start.py
+++ tg_bot/handlers/user/start/start.py
@@ -1,7 +1,10 @@
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from fluent.runtime import FluentLocalization
+
+from api.books_base_api.schemas import UserSchema
+from tg_bot.services import set_user_commands
 
 command_start_router = Router()
 
@@ -10,11 +13,17 @@ command_start_router = Router()
 async def start(
     message: Message,
     l10n: FluentLocalization,
+    bot: Bot,
+    user: UserSchema,
 ):
-    full_name = message.from_user.full_name
     await message.answer(
         l10n.format_value(
             "start",
-            {"full_name": full_name},
+            {"full_name": user.full_name},
         )
+    )
+    await set_user_commands(
+        bot=bot,
+        id_user=user.id_user,
+        is_admin=True if user.is_admin else False,
     )
